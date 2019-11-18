63760bc4b2b815355120558e9ac85a18482094d3
WuRoc <48498551+WuRoc@users.noreply.github.com>
2019-11-18 21:57:27 +0800

    2019-11-18  吴贺朋

--- src/com/whp/test/HelloWorld.java
+++ src/com/whp/test/HelloWorld.java
@@ -2,6 +2,6 @@ package com.whp.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("你好啊 ！ introduction to information security technology");
+        System.out.println("你真好啊 ！ introduction to information security technology");
     }
 }
