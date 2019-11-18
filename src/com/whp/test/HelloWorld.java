dbaf10493ed5dee8cd2ac07475d359f782f0f9bc
WuRoc <48498551+WuRoc@users.noreply.github.com>
2019-11-18 20:54:53 +0800

    Initial commit

---
+++ src/com/whp/test/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.whp.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好！ introduction to information security technology");
+    }
+}
